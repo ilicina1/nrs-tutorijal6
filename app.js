b7349df9c0eecc82a5673b69b8759ce9f5e77e14
ilicina1 <ilhan.licina@outlook.com>
2021-05-06 23:07:42 +0200

    push nodejs aplikacije sa listom gradova za pripremu tutorijala6

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static("public"));
+app.use(express.static(__dirname));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/public/" + "index.html");
+});
+
+app.listen(3000);
